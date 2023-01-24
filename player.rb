cf45c18a143c036d250c5d104505311f3f1a5d86
Hilora Dewett <h.dewett@outlook.com>
2023-01-24 13:02:29 -0500

    Add a class to initialize players for the game.

--- player.rb
+++ player.rb
@@ -1,1 +1,18 @@
-initial commit
+class Player
+  attr_reader :name, :lives_left, :points_scored
+  attr_writer :lives_left, :points_scored
+
+  def initialize(name)
+    @name = name
+    @lives_left = 3
+    @points_scored = 0
+  end
+
+  def addPoint
+    @points_scored += 1
+  end
+
+  def loseLife
+    @lives_left -=1
+  end
+end
