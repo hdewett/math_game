41ba7c996401cf6dea000b6496ba42e58848aa29
Hilora Dewett <h.dewett@outlook.com>
2023-01-24 15:29:30 -0500

    Code cleanup.

--- player.rb
+++ player.rb
@@ -1,15 +1,9 @@
 class Player
-  attr_reader :name, :lives_left, :points_scored
-  attr_writer :lives_left, :points_scored
+  attr_reader :name, :lives_left
 
   def initialize(name)
     @name = name
     @lives_left = 3
-    @points_scored = 0
-  end
-
-  def add_point
-    @points_scored += 1
   end
 
   def lose_life
